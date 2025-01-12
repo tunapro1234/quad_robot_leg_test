41151550b19f090d04451ab2aed9cd9dfa7674b5
Tuna Gül <tuna.gul@nfrproducts.com>
2025-01-12 15:04:30 +0300

    add jitter to rpm and torque

--- scripts/theoretical.py
+++ scripts/theoretical.py
@@ -60,7 +60,8 @@ def get_motor_torque(current):
         I = no_load_current + current_slope * torque
     =>  torque = (I - no_load_current) / current_slope
     """
-    return (current - no_load_current) / current_slope
+    torque = (current - no_load_current) / current_slope
+    return apply_jitter(torque, 0.1)
 
 
 def get_motor_rpm(current):
@@ -72,7 +73,7 @@ def get_motor_rpm(current):
     """
     torque = get_motor_torque(current)
     rpm = no_load_speed - motor_reg * torque
-    return rpm
+    return apply_jitter(rpm, 0.1)
 
 
 def get_motor_power(current):
